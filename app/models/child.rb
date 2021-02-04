d31ca949c2c79e409a29d871686cd710759ba88f
Andy <Achen725.cfl@gmail.com>
2021-02-04 11:30:06 -0500

    Added name method

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,8 +1,15 @@
 class Child < ApplicationRecord
     
+    #Relationships
     has_many :chores
     has_many :tasks, through: :chores
 
+    #Validations
     validates_presence_of :first_name, :last_name
+
+    #Methods
+    def name
+        first_name + " " + last_name
+    end
 
 end
