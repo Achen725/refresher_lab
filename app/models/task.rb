de1f339ac2b01468ef329260359363e02875ef6b
Andy <Achen725.cfl@gmail.com>
2021-02-04 11:52:34 -0500

    Added scopes to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,5 +5,11 @@ class Task < ApplicationRecord
 
     #Validations
     validates_presence_of :name
+    validate_numericality_of :points, :only_integer => true
 
+    #Scopes
+    scope :alphabetical, -> { order('name') }
+    scope :active, -> { where(active: true) }
+  
+     
 end
