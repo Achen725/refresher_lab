aecb4c25d1411e84ce14a61979b3e0c1b879aae1
Andy <Achen725.cfl@gmail.com>
2021-02-04 12:05:06 -0500

    Added scopes for past and upcoming in chore model

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -8,10 +8,11 @@ class Chore < ApplicationRecord
     validates_date :due_on
 
     #Scope
-
+    scope :by_task, -> { joins(:task).order('tasks.name') }
     scope :past, -> { where('due_on < ?', Date.today.to_date) }
     scope :upcoming, -> { where('due_on >= ?', Date.today.to_date) }
     scope :chronological, -> { joins(:task).order('due_on', 'completed') }
     scope :pending, -> { where(completed: false) }
     scope :done, -> { where(completed: true) }
+
 end
